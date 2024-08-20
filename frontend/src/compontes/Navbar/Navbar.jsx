da6e45f1c03729e54864b3f3ff1277db39ffc7f9
SadeshWijekoon <10898706@students.plymouth.ac.uk>
2024-08-20 12:41:40 +0530

    add relevent css (1.0)

--- frontend/src/compontes/Navbar/Navbar.jsx
+++ frontend/src/compontes/Navbar/Navbar.jsx
@@ -1,10 +1,25 @@
 import './Navbar.css'
-
+import {assets} from '../../assets/assets'
 
 const Navbar = () => {
   return (
     <div className='navbar'>
-        
+        <img src={assets.logo} alt="" className="logo" />
+        <ul className="navber-menu">
+          <li>Home</li>
+          <li>Menu</li>
+          <li>Mobile-App</li>
+          <li>Contact-Us</li>
+        </ul>
+        <div className="navbar-right">
+          <img src={assets.search_icon} alt=''/>
+           <div className="navbar-search-icon">
+             <img src={assets.basket_icon} alt=''/>
+               <div className="dot">
+                 <button>Sigh In</button>
+               </div>
+           </div>
+        </div>
     </div>
   )
 }
