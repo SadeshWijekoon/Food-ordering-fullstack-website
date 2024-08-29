1b5f8e1d3a7c7d1be37b5a737acc1e1ea35707b6
SadeshWijekoon <10898706@students.plymouth.ac.uk>
2024-08-29 22:25:06 +0530

    add the download now comont and make navbar responsive

--- frontend/src/compontes/App downlod/App download.jsx
+++ frontend/src/compontes/App downlod/App download.jsx
@@ -1,8 +1,15 @@
-
+import { assets } from '../../assets/assets';
+import './App download.css'
 
 const Appdownload = () => {
   return (
-    <div>App download</div>
+    <div className='app-download' id='app-download'>
+      <p>We are avalable on <br/> Tomato App</p> 
+      <div className='app-dow-plat'>
+        <img src={assets.play_store}/>
+        <img src={assets.app_store}/>
+      </div>
+    </div>
   )
 }
 
--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -3,6 +3,7 @@ import Exploremenu from '../../compontes/Exploremenuj/Exploremenu';
 import Header from '../../compontes/Header/Header';
 import './Home.css'
 import Fooddisplay from '../../compontes/Fooddisplay/Fooddisplay';
+import Appdownload from '../../compontes/App downlod/App download';
 
 
 const Home = () => {
@@ -15,6 +16,7 @@ const [category,setCategory]=useState('All')
       <Header/>
       <Exploremenu category={category} setCategory={setCategory}/>
       <Fooddisplay category={category}/>
+      <Appdownload/>
     </div>
     
     </>
