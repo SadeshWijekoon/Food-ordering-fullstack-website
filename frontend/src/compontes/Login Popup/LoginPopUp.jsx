74b45d53e6bbf5894de380a9dc5941e00b5cb8cf
SadeshWijekoon <10898706@students.plymouth.ac.uk>
2024-10-08 13:15:40 +0530

    update the nav bar

--- frontend/src/compontes/Login Popup/LoginPopUp.jsx
+++ frontend/src/compontes/Login Popup/LoginPopUp.jsx
@@ -1,34 +1,50 @@
-import { useState } from 'react';
-import'./LoginPopUp.css'
-import { assets } from '../../assets/assets';
+import { useState } from "react";
+import "./LoginPopUp.css";
+import { assets } from "../../assets/assets";
 
-const LoginPopUp = ({setShowLogin}) => {
- const  [currentState,setCurrentState] =useState("sign up")
+const LoginPopUp = ({ setShowLogin }) => {
+  const [currentState, setCurrentState] = useState("sign up");
 
   return (
-    <div className='login-popup'>
-      <form className='login-popup-con'>
+    <div className="login-popup">
+      <form className="login-popup-con">
         <div className="log-pop-tit">
           <h2>{currentState}</h2>
-          <img onClick={()=>setShowLogin(false)} src={assets.cross_icon}></img>
+          <img
+            onClick={() => setShowLogin(false)}
+            src={assets.cross_icon}
+          ></img>
         </div>
         <div className="login-pop-int">
-          {currentState==='login'?<></>:<input type='text' placeholder='Your name' required></input>}
-          <input type='email' placeholder='Your email' required></input>
-          <input type='password' placeholder='Password' required></input>
+          {currentState === "login" ? (
+            <></>
+          ) : (
+            <input type="text" placeholder="Your name" required></input>
+          )}
+          <input type="email" placeholder="Your email" required></input>
+          <input type="password" placeholder="Password" required></input>
         </div>
-        <button>{currentState==='sign up'?'create account':'login'}</button>
-        <div className='log-pop-cond'>
-          <input type='checkbox' required/>
+        <button>
+          {currentState === "sign up" ? "create account" : "login"}
+        </button>
+        <div className="log-pop-cond">
+          <input type="checkbox" required />
           <p>By continunig,i agree to the term of use & privacy policy</p>
         </div>
-        {currentState==='login'?<p>Create a new account? <span onClick={()=>setCurrentState('sigh up')}>Click here</span></p>:
-        <p>Already have an account? <span onClick={()=>setCurrentState('login')}>Login here</span></p>}
-        
-        
+        {currentState === "login" ? (
+          <p>
+            Create a new account?{" "}
+            <span onClick={() => setCurrentState("sigh up")}>Click here</span>
+          </p>
+        ) : (
+          <p>
+            Already have an account?{" "}
+            <span onClick={() => setCurrentState("login")}>Login here</span>
+          </p>
+        )}
       </form>
     </div>
-  )
-}
+  );
+};
 
 export default LoginPopUp;
